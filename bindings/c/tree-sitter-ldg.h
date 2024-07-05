4e1595b6373ab9169644c4a1ad88a5a18ae80861
lesserfish <lesserfish@pm.me>
2024-07-05 02:31:06 -0300

    first commit

---
+++ bindings/c/tree-sitter-ldg.h
@@ -0,0 +1,16 @@
+#ifndef TREE_SITTER_LDG_H_
+#define TREE_SITTER_LDG_H_
+
+typedef struct TSLanguage TSLanguage;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+const TSLanguage *tree_sitter_ldg(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // TREE_SITTER_LDG_H_
---
+++ bindings/python/tree_sitter_ldg/__init__.py
@@ -0,0 +1,5 @@
+"Ldg grammar for tree-sitter"
+
+from ._binding import language
+
+__all__ = ["language"]
